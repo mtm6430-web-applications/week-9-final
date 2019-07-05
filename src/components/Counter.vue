162b19bcb355303e5aaf93ce772ae238fc310638
Abhay Sharma <abhaysharmawebd@gmail.com>
2019-07-05 10:28:27 -0400

    added state management basics

---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p>Counter value is {{ counter }}</p>
+    <p>Double coutner is {{ counterTwo }}</p>
+    <button @click="decrement">Decrement</button>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+    counterTwo() {
+      return this.$store.getters.doubleCount;
+    }
+  },
+  methods: {
+    increment() {
+      // this.$store.state.counter++;
+
+      // instead of modifying the state directly we commit a mutation in the store to take care of this
+      // this.$store.commit("increment");
+
+      // we use actions in our comonents which commit the mutation to modify our state
+      this.$store.dispatch("increment");
+    },
+    decrement() {
+      // this.$store.state.counter--;
+
+      // instead of modifying the state directly we commit a mutation in the store to take care of this
+      // this.$store.commit("decrement");
+
+      this.$store.dispatch("decrement");
+    }
+  }
+};
+</script>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,30 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    counter: 2
+  },
+  getters: {
+    doubleCount: state => {
+      return state.counter * 2;
+    }
+  },
+  mutations: {
+    increment: state => {
+      state.counter++;
+    },
+    decrement: state => {
+      state.counter--;
+    }
+  },
+  actions: {
+    // mutations are synchronous, which means they will run right away. We can use asynchronus code by using actions
+    // Actions are used to commit mutations on the state
+    increment: context => {
+      context.commit("increment");
+    },
+    decrement: context => {
+      context.commit("decrement");
+    }
+  }
 });
